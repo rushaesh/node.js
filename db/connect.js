bedc7c9c18782b66d1f180e8a146ba8aa82fe2b0
rushaesh <aeshavastapara05@gmail.com>
2025-03-02 20:58:00 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -4,10 +4,8 @@ const connectdb = async () => {
     try {
         console.log("Connecting to MongoDB...");
         
-        await mongoose.connect(process.env.MONGO_URI,{
-             useNewUrlParser: true,
-    useUnifiedTopology: true,
-        }); // Use correct variable
+        await mongoose.connect(process.env.MONGO_URI,
+        ); // Use correct variable
 
         console.log("MongoDB Connected Successfully");
     } catch (error) {
